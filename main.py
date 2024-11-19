330c583e539aa52efbdef3faadcc463d918f9f37
Mehrad Hassanpour <mehradhassanp@gmail.com>
2024-11-19 11:06:02 -0600

    Add initial project files: .gitignore, main.py, and requirements.txt

---
+++ main.py
@@ -0,0 +1,12 @@
+import torch
+from torch import nn
+import torchvision
+from torchvision import transforms
+
+# load CIFAR-10 dataset with pytorch
+# convert to tensor, normalize and flatten
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2470, 0.2435, 0.2616)),
+    transforms.Lambda(lambda x: torch.flatten(x)),
+])
